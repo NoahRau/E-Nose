2853735fe1f90a57ed561cb4ded6bd09092b4f83
Raupold, Noah <noah.raupold@study.thws.de>
2026-01-27 14:05:45 +0100

    Sources

--- ModelTraining/src/ModelTraining/__init__.py
+++ ModelTraining/src/ModelTraining/__init__.py
@@ -6,6 +6,7 @@ for the E-Nose anomaly detection system using MAE and DINOv2.
 
 from ModelTraining.dataset import FridgeDataset
 from ModelTraining.model import FridgeMoCA
-from ModelTraining.model_advanced import FridgeMoCA_Pro
+# Change 'FridgeMoCA_Pro' to 'FridgeMoCA_V3' below:
+from ModelTraining.model_advanced import FridgeMoCA_V3
 
-__all__ = ["FridgeDataset", "FridgeMoCA", "FridgeMoCA_Pro"]
+__all__ = ["FridgeDataset", "FridgeMoCA", "FridgeMoCA_V3"]
